ef1f6fe60207375bcc4fdcb7a1eb6c7eb8c4edc5
bukhaled2002 <bakhaled.310@gmail.com>
2023-10-15 17:41:11 -0700

    ff

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,10 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
 
     def __str__(self):
         """printing the value"""
