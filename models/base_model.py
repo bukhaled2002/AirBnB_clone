172214b9688d4ce09c2c0d8ec27b07739a8a0829
bukhaled2002 <bakhaled.310@gmail.com>
2023-10-15 17:26:04 -0700

    f

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ the base model"""
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -12,14 +13,6 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tf)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
 
     def __str__(self):
         """printing the value"""
@@ -28,7 +21,7 @@ class BaseModel:
  
     def save(self):
         """ save the file """
-        self.updated_at = datetime.todayi()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """return the all dictionary"""
